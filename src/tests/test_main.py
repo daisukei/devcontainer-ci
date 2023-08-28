3fc328dc83d462b86b00223cd8777dfcbd99b967
Daisuke Inoue <d_inoue777@hotmail.co.jp>
2023-08-28 10:49:38 +0900

    Update test_main.py

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -9,3 +9,6 @@ def test_agecheck():
     assert agecheck(17) == "You are underage."
     assert agecheck(18) == "You just became an adult."
     assert agecheck(19) == "You are an adult."
+
+def test_agecheck_fail():
+    assert agecheck(17) == "This is mistake."
